bee26d252a0e161306554737b0b5655aa56f9ccf
Ilia Frolov <ilia_frolov@list.ru>
2023-11-29 19:45:57 +0300

    Закончили первую часть

--- app/src/main/java/com/example/todo_app/MainActivity.kt
+++ app/src/main/java/com/example/todo_app/MainActivity.kt
@@ -1,11 +1,43 @@
 package com.example.todo_app
 
+import android.app.Dialog
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.ImageView
+import com.example.todo_app.databinding.ActivityMainBinding
+import com.example.todo_app.utils.setupDialog
 
 class MainActivity : AppCompatActivity() {
+
+    private val mainBinding: ActivityMainBinding by lazy {
+        ActivityMainBinding.inflate(layoutInflater)
+    }
+
+    private val addTaskDialog : Dialog by lazy {
+        Dialog(this, R.style.DialogCustomTheme).apply {
+            setupDialog(R.layout.add_task_dialog)
+        }
+    }
+
+    private val updateTaskDialog : Dialog by lazy {
+        Dialog(this, R.style.DialogCustomTheme).apply {
+            setupDialog(R.layout.update_task_dialog)
+        }
+    }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContentView(mainBinding.root)
+
+
+        val addCloseImg = addTaskDialog.findViewById<ImageView>(R.id.closeImg)
+        val updateCloseImg = updateTaskDialog.findViewById<ImageView>(R.id.closeImg)
+
+        addCloseImg.setOnClickListener { addTaskDialog.dismiss() }
+        updateCloseImg.setOnClickListener { updateTaskDialog.dismiss() }
+
+        mainBinding.addTaskFABtn.setOnClickListener {
+            addTaskDialog.show()
+        }
     }
 }
